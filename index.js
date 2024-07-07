6e18da5d7c5e825e5671588e0fae14071ab91055
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 17:04:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+
+function findMatching(drivers, searchTerm) {
+    return drivers.filter(driver => driver.toLowerCase() === searchTerm.toLowerCase());
+} 
+  
+  function fuzzyMatch(drivers, searchTerm) {
+    const searchTermLowerCase = searchTerm.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchTermLowerCase));
+  }
+  function matchName(drivers, searchTerm) {
+    const searchTermLowerCase = searchTerm.toLowerCase();
+    return drivers.filter(driver => driver.name.toLowerCase() === searchTermLowerCase);
+  }
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+  
+  const matchingDrivers = findMatching(drivers, 'Bobby'); 
+  const startingMatches = fuzzyMatch(drivers, 'Sa'); 
+  
+  const driverObjects = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+  ];
+  
+  const namedMatches = matchName(driverObjects, 'bobby'); 
+  
